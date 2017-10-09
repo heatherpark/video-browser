19f4eef7579a8dfc23ac002e0156fca2f63c33f8
Heather Park <hthr.prk@gmail.com>
2017-10-09 10:59:27 -0700

    Set selected video in YTSearch callback

--- src/index.js
+++ src/index.js
@@ -15,7 +15,10 @@ class App extends Component {
     };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-      this.setState({ videos });
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -23,7 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.selectedVideo} />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>  
     );
